690d5f9d9eec11e318723489481b727d8a720ec0
ujwal <`ujwal.yangalwar@decimalpointanalytics.com>
2023-11-22 12:10:00 +0530

    added share for coping url

--- components/SideDrawer.tsx
+++ components/SideDrawer.tsx
@@ -5,9 +5,20 @@ const BASE_URL_PHOTO = "https://maps.googleapis.com/maps/api/place/photo?maxwidt
 
 function SideDrawer({place, close }: any) {
 
+    const CURRENT_URL = 'https://www.google.com/maps/search/?api=1&query='+place.name+place.formatted_address
+
     const handleDirectionClick=()=>{
-        window.open('https://www.google.com/maps/search?api=1&query='+place.name+place.formatted_address)
+        window.open(CURRENT_URL)
     }
+
+    const copyToClipboard = async(url:any) => {
+        try {
+                await navigator.clipboard.writeText(url);
+                alert('Copied to Clipboard');
+            } catch (err) {
+                console.error('Unable to copy to clipboard', err);
+            }
+        };
 
   return (
     <div className='h-screen w-screen md:w-[500px] bg-purple-100 shadow-md p-5' >
@@ -19,7 +30,7 @@ function SideDrawer({place, close }: any) {
             <h2 className='line-clamp-2 text-2xl'>{place.name}</h2>
 
             <Image 
-                className='w-full mt-3 h-[200px] object-cover rounded-xl' 
+                className='w-full mt-3 h-[270px] object-cover rounded-xl' 
                 src={BASE_URL_PHOTO+place?.photos[0]?.photo_reference+"&key="+process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY} 
                 alt="placeholder" 
                 width={200} 
@@ -51,7 +62,9 @@ function SideDrawer({place, close }: any) {
                     <span className='text-lg'>Direction</span>
                 </button>
                 
-                <button className='flex flex-1 gap-4 justify-center items-center bg-[#d58bea] pr-3 py-1.5 rounded-lg'>
+                <button 
+                    onClick={() => copyToClipboard(CURRENT_URL)}
+                    className='flex flex-1 gap-4 justify-center items-center bg-[#d58bea] pr-3 py-1.5 rounded-lg'>
                     <svg width="36px" height="36px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M13.803 5.33333C13.803 3.49238 15.3022 2 17.1515 2C19.0008 2 20.5 3.49238 20.5 5.33333C20.5 7.17428 19.0008 8.66667 17.1515 8.66667C16.2177 8.66667 15.3738 8.28596 14.7671 7.67347L10.1317 10.8295C10.1745 11.0425 10.197 11.2625 10.197 11.4872C10.197 11.9322 10.109 12.3576 9.94959 12.7464L15.0323 16.0858C15.6092 15.6161 16.3473 15.3333 17.1515 15.3333C19.0008 15.3333 20.5 16.8257 20.5 18.6667C20.5 20.5076 19.0008 22 17.1515 22C15.3022 22 13.803 20.5076 13.803 18.6667C13.803 18.1845 13.9062 17.7255 14.0917 17.3111L9.05007 13.9987C8.46196 14.5098 7.6916 14.8205 6.84848 14.8205C4.99917 14.8205 3.5 13.3281 3.5 11.4872C3.5 9.64623 4.99917 8.15385 6.84848 8.15385C7.9119 8.15385 8.85853 8.64725 9.47145 9.41518L13.9639 6.35642C13.8594 6.03359 13.803 5.6896 13.803 5.33333Z" fill="#2a2b32"></path> </g></svg>
                     <span className='text-lg text-center'>Share</span>
                 </button>
@@ -59,7 +72,7 @@ function SideDrawer({place, close }: any) {
 
             <div className='mt-2'>
                 <iframe
-                    className='w-full h-[250px] rounded-xl'
+                    className='w-full h-[280px] rounded-xl'
                     width={600}
                     height={450}
                     loading="lazy"
